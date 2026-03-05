4e3baf4327899f40cedbdb283891949a214ed8f7
Your Name <your@email.com>
2026-03-05 13:16:59 +0530

    Updated backend API for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://login-backend-r91o.onrender.com"
 });
 
 export const loginUser = (data) => API.post("/login", data);
